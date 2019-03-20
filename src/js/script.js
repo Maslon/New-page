d2172f2f825e2537093946cdaa4d38ff99f2d879
maslo <radek.fr@email.cz>
2019-03-20 21:17:20 +0100

    cahnging some colors

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,35 @@
-import "../scss/main.scss"
-import "./localize.js"
+import '../scss/main.scss';
+import './localize.js';
 
-const menuBtn = document.querySelector(".menu-button")
-const menu = document.querySelector(".menu")
-const menuBrand = document.querySelector(".menu-brand")
-const menuNav = document.querySelector(".menu-nav")
-const menuItems = document.querySelectorAll(".menu-item")
-const langButtons = document.querySelector(".lang-buttons")
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+const menuBrand = document.querySelector('.menu-brand');
+const menuNav = document.querySelector('.menu-nav');
+const menuItems = document.querySelectorAll('.menu-item');
+const langButtons = document.querySelector('.lang-buttons');
+const menuLines = document.querySelectorAll('.menu-line');
 
 let opened = false;
 
-menuBtn.addEventListener("click", menuToggle)
-menu.addEventListener("click", menuToggle)
+menuBtn.addEventListener('click', menuToggle);
+menu.addEventListener('click', menuToggle);
 
-
-function menuToggle(){
-    opened = !opened
-    if(opened){
-        menuBtn.classList.add("hide")
-        menu.classList.add("show")
-        menuBrand.classList.add("show")
-        menuNav.classList.add("show")
-        langButtons.classList.add("show")
-        menuItems.forEach(item => item.classList.add("show"))
-    } else {
-        menuBtn.classList.remove("hide")
-        menu.classList.remove("show")
-        menuBrand.classList.remove("show")
-        menuNav.classList.remove("show")
-        langButtons.classList.remove("show")
-        menuItems.forEach(item => item.classList.remove("show"))
-    }
-    console.log(opened)
+function menuToggle() {
+	opened = !opened;
+	if (opened) {
+		menuBtn.classList.add('hide');
+		menu.classList.add('show');
+		menuBrand.classList.add('show');
+		menuNav.classList.add('show');
+		langButtons.classList.add('show');
+		menuItems.forEach(item => item.classList.add('show'));
+	} else {
+		menuBtn.classList.remove('hide');
+		menu.classList.remove('show');
+		menuBrand.classList.remove('show');
+		menuNav.classList.remove('show');
+		langButtons.classList.remove('show');
+		menuItems.forEach(item => item.classList.remove('show'));
+	}
+	console.log(opened);
 }
-
-
-
-
